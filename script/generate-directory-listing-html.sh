fe2a0757f09a08d753865377b81064d74a01b44d
garethfoote <gareth.foote@gmail.com>
2021-03-19 09:44:36 +0000

    Media list

--- script/generate-directory-listing-html.sh
+++ script/generate-directory-listing-html.sh
@@ -17,7 +17,7 @@ for filepath in "${arr[@]}"; do
   echo "${ROOT}${filepath}"
   for i in `find "$ROOT/$filepath" -maxdepth 1 -mindepth 1 -type f | sort`; do
     file=`basename "$i"`
-    echo "    <li><a href=\"/$path/$file\">$file</a></li>" >> $OUTPUT
+    echo "    <li><a href=\"https://intobodmin.github.io/all-the-beautiful-things/$path/$file\">https://intobodmin.github.io/all-the-beautiful-things/$path/$file</a></li>" >> $OUTPUT
   done
   echo "  </ul>" >> $OUTPUT
 done
